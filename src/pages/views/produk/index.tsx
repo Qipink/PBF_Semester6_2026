ea64f65d4f092f87700d692720ce7954b519709d
Muhammad Rifqi Rizqullah <139708627+Qipink@users.noreply.github.com>
2026-04-01 13:27:27 +0700

    Jobsheet 8 Step 3 - Implementasi Skeleton Loading

--- src/pages/views/produk/index.tsx
+++ src/pages/views/produk/index.tsx
@@ -16,14 +16,25 @@ const TampilanProduk = ({ products }: { products: ProductType[] }) => {
     <div className={styles.product}>
       <h1 className={styles.product__title}>Daftar Produk</h1>
       <div className={styles.product__content}>
-        {products.map((products: ProductType) => (
-          <div key={products.id} className={styles.product__content__item}>
-            <img src={products.image} alt={products.name} className={styles.product__content__item__image}/>
-            <p className={styles.product__content__item__category}>Kategori: {products.category}</p>
-            <h2 className={styles.product__content__item__name}>Nama: {products.name}</h2>
-            <p className={styles.product__content__item__price}>Harga: Rp. {products.price}</p>
+        {products.length > 0 ? (
+          <>
+            {products.map((products: ProductType) => (
+              <div key={products.id} className={styles.product__content__item}>
+                <img src={products.image} alt={products.name} className={styles.product__content__item__image}/>
+                <p className={styles.product__content__item__category}>Kategori: {products.category}</p>
+                <h2 className={styles.product__content__item__name}>Nama: {products.name}</h2>
+                <p className={styles.product__content__item__price}>Harga: Rp. {products.price}</p>
+              </div>
+            ))}
+          </>
+        ) : (
+          <div className={styles.product__content__skeleton}>
+            <div className={styles.product__content__skeleton__image}></div>
+            <div className={styles.product__content__skeleton__category}></div>
+            <div className={styles.product__content__skeleton__name}></div>
+            <div className={styles.product__content__skeleton__price}></div>
           </div>
-        ))}
+        )}
       </div>
     </div>
   )
