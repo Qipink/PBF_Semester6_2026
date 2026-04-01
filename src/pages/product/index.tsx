59a9610f1b8369ff11090b939dfeea0fdf43aad7
Muhammad Rifqi Rizqullah <139708627+Qipink@users.noreply.github.com>
2026-04-01 13:35:06 +0700

    Jobsheet 8 Step 5 - Implementasi SWR

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,25 +1,18 @@
 import { useEffect, useState } from 'react';
 import TampilanProduk from '../views/produk';
+import useSWR from 'swr';
+import fetcher from '../utils/swr/fetcher';
+
+// const fetcher = (url:string) => fetch(url).then((res) => res.json());
 
 const kategori = () => {
   const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    fetch("/api/produk")
-      .then((response) => response.json())
-      .then((responsedata) => {
-        setProducts(responsedata.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching product:", error);
-      })
-  }, []);
+  const { data, error, isLoading } = useSWR('/api/produk', fetcher);
 
   return (
     <div>
-      <TampilanProduk products={products} />
+      <TampilanProduk products={isLoading ? [] : data.data} />
     </div>
-  );
-};
-
+  )
+}
 export default kategori;
