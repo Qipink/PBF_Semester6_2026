adcdb57183b0f8022e919f7faa157fdf3c9af145
Muhammad Rifqi Rizqullah <139708627+Qipink@users.noreply.github.com>
2026-04-06 21:20:03 +0700

    Jobsheet 11 - DYNAMIC ROUTING & STATIC GENERATION

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -23,9 +23,34 @@ const HalamanProduk = ({product} : {product: ProductType}) => {
 
 export default HalamanProduk;
 
-export async function getServerSideProps({ params }: { params: {id: string} }) {
-    const res = await fetch(`http://localhost:3000/api/produk/${params?.id}`);
+// export async function getServerSideProps({ params }: { params: {id: string} }) {
+//     const res = await fetch(`http://localhost:3000/api/produk/${params?.id}`);
+//     const response = await res.json();
+
+//     return {
+//         props: {
+//             product: response.data,
+//         }
+//     }
+// }
+
+export async function getStaticPaths() {
+    const res = await fetch(`http://localhost:3000/api/produk`)
     const response = await res.json();
+
+    const paths = response.data.map((product: ProductType) => ({
+        params: { id: product.id}
+    }));
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({params}:{params:{id:string}}) {
+    const res = await fetch(`http://localhost:3000/api/produk/${params?.id}`);
+    const response: { data: ProductType[] } = await res.json();
 
     return {
         props: {
