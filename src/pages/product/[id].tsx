2c59d9c0e2a1c960e4d8bb5bfce5e3a7e7459370
Muhammad Rifqi Rizqullah <139708627+Qipink@users.noreply.github.com>
2026-04-06 20:30:22 +0700

    Jobsheet 11 Step 3 - Implementasi SSR

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -2,16 +2,34 @@ import { useRouter } from "next/router";
 import useSWR from "swr";
 import fetcher from "@/utils/swr/fetcher";
 import DetailProduk from "../../views/DetailProduct";
+import { ProductType } from "@/types/Product.type";
 
-const HalamanProduk = () => {
+const HalamanProduk = ({product} : {product: ProductType}) => {
 
-    const {query} = useRouter();
-    const{data, error, isLoading} = useSWR(`/api/produk/${query.id}`, fetcher);
+    // const {query} = useRouter();
+    // const{data, error, isLoading} = useSWR(`/api/produk/${query.id}`, fetcher);
+    // return (
+    //     <div>
+    //         <DetailProduk products={isLoading ? [] : data.data}/>
+    //     </div>
+    // )
+
     return (
         <div>
-            <DetailProduk products={isLoading ? [] : data.data}/>
+            <DetailProduk products={product}/>
         </div>
     )
 };
 
 export default HalamanProduk;
+
+export async function getServerSideProps({ params }: { params: {id: string} }) {
+    const res = await fetch(`http://localhost:3000/api/produk/${params?.id}`);
+    const response = await res.json();
+
+    return {
+        props: {
+            product: response.data,
+        }
+    }
+}
