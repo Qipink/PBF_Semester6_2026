4ead80dbb64acca2641a5ae83eeb6e9531ea40b5
Muhammad Rifqi Rizqullah <139708627+Qipink@users.noreply.github.com>
2026-04-08 13:13:33 +0700

    Jobsheet 14 Step 5 - Tambahkan Tombol Login & Logout

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -1,11 +1,18 @@
 import styles from "./navbar.module.css";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Navbar = () => {
+    const { data } = useSession();
     return (
         <div className={styles.navbar}>
             <div className="big">
                 <h1>Navbar Component</h1>
             </div>
+            {data ? (
+                <button onClick={() => signOut()}>Sign Out</button>
+            ) : (
+                <button onClick={() => signIn()}>Sign In</button>
+            )}
         </div>
     )
 }
