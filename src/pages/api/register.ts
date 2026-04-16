f38f110352520848bf7d67d696bd332bf96a31d2
Muhammad Rifqi Rizqullah <139708627+Qipink@users.noreply.github.com>
2026-04-16 23:12:01 +0700

    Jobsheet 15 Step 2 - Membuat API Register

---
+++ src/pages/api/register.ts
@@ -0,0 +1,24 @@
+import { signUp } from '@/utils/db/servicefirebase';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+    name: string,
+    alamat: string,
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    if (req.method === "POST") {
+        await signUp (req.body, (response: { status: string; message: string }) => {
+            if (response.status === "success") {
+                res.status(200).json({ name: response.message, alamat: "Alamat berhasil disimpan" });
+            } else {
+                res.status(400).json({ name: response.message, alamat: "Gagal menyimpan alamat" });
+            }
+        });
+    } else {
+        res.status(405).json({ name: "Method Not Allowed", alamat: "Hanya metode POST yang diizinkan" });
+    }
+}
