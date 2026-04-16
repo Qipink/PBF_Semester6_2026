c8b050886aae2b252002258169593a119f958eab
Muhammad Rifqi Rizqullah <139708627+Qipink@users.noreply.github.com>
2026-04-16 15:20:12 +0700

    Jobsheet 15 Step 1 - Membuat Register View

--- src/views/auth/register/index.tsx
+++ src/views/auth/register/index.tsx
@@ -1,11 +1,39 @@
 import Link from "next/link";
-import styles from "./register.module.css";
+import styles from "./register.module.scss";
 
 const TampilanRegister = () => {
     return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>Halaman Register</h1>
-            <Link href="/auth/login" className={styles.link}>Ke Halaman Login</Link>
+        <div className={styles.register}>
+            <h1 className={styles.register__title}>Halaman Register</h1>
+            <div className={styles.register__form}>
+                <form action="">
+                    <div className={styles.register__form__item}>
+                        <label htmlFor="email" className={styles.register__form__item__label}>
+                            Email
+                        </label>
+                        <input type="email" id="email" name="email" placeholder="Email" className={styles.register__form__item__input} />
+                    </div>
+                    <div className={styles.register__form__item}>
+                        <label htmlFor="Fullname" className={styles.register__form__item__label}>
+                            Fullname
+                        </label>
+                        <input type="text" id="Fullname" name="Fullname" placeholder="Fullname" className={styles.register__form__item__input} />
+                    </div>
+                    <div className={styles.register__form__item}>
+                        <label htmlFor="Fullname" className={styles.register__form__item__label}>
+                            Password
+                        </label>
+                        <input type="password" id="Password" name="Password" placeholder="Password" className={styles.register__form__item__input} />
+                    </div>
+                    <button type="submit" className={styles.register__form__item__button}>
+                        Register
+                    </button>
+                </form>
+                <br />
+                <p className={styles.register__form__item__text}>
+                    Sudah punya akun? <Link href="/auth/login">Ke Halaman Login</Link>
+                </p>
+            </div>
         </div>
     )
 }
