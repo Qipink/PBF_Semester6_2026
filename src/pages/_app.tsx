ca691cd70b30514976e3bcb030e2ee5b837ec62d
Muhammad Rifqi Rizqullah <139708627+Qipink@users.noreply.github.com>
2026-04-22 13:13:51 +0700

    Jobsheet 18 Tugas 3 - Tambahkan script Google Analytics menggunakan next/script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react';
 import { Poppins } from 'next/font/google';
+import Script from 'next/script';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -11,8 +12,26 @@ const poppins = Poppins({
 });
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const gaId = process.env.NEXT_PUBLIC_GA_ID;
+
   return (
     <SessionProvider session={session}>
+      {gaId ? (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gaId}');
+            `}
+          </Script>
+        </>
+      ) : null}
       <div className={poppins.className}>
         <AppShell>
           <Component {...pageProps} />
