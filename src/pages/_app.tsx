cf7b8e26f77fd8b06a0e085550c43b96dae5d80b
Muhammad Rifqi Rizqullah <139708627+Qipink@users.noreply.github.com>
2026-04-22 13:11:41 +0700

    Jobsheet 18 Tugas 2 - Gunakan minimal 1 font dari next/font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,22 @@ import AppShell from '@/components/layout/Appshell'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  display: 'swap',
+});
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <AppShell>
-        <Component {...pageProps} />
-      </AppShell>
+    <SessionProvider session={session}>
+      <div className={poppins.className}>
+        <AppShell>
+          <Component {...pageProps} />
+        </AppShell>
+      </div>
     </SessionProvider>
   )
 
