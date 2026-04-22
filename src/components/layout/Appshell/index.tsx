10b666829fd4c22d4b37f2f20109c454bc5116b2
Muhammad Rifqi Rizqullah <139708627+Qipink@users.noreply.github.com>
2026-04-22 12:52:44 +0700

    Jobsheet 18 Step 3 - Font Optimization

--- src/components/layout/Appshell/index.tsx
+++ src/components/layout/Appshell/index.tsx
@@ -1,12 +1,18 @@
 import { useRouter } from "next/router";
 import Footer from "../footer";
 import Navbar from "../navbar";
+import { Roboto } from "next/font/google";
 
 const disableNavbar = ['/auth/login', '/auth/register', '/404'];
 
 type AppShellProps = {
     children: React.ReactNode;
 }
+
+const roboto = Roboto({
+    subsets: ["latin"],
+    weight: ["400", "500", "700"],
+})
 
 const AppShell = (props: AppShellProps) => {
     const { children } = props;
@@ -15,7 +21,7 @@ const AppShell = (props: AppShellProps) => {
     console.log(router);
 
     return (
-        <main>
+        <main className={roboto.className}>
             {!disableNavbar.includes(pathname) && <Navbar/>}
             {children}
 
